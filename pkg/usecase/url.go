95cbf6d4310fdd5a66a3577ebab3ff2b9772214d
Mayank Mohan <mayankmohan@Mayanks-MacBook-Air.local>
2023-12-28 09:15:56 +0530

    caching introduced before fetching from DB

--- pkg/usecase/url.go
+++ pkg/usecase/url.go
@@ -6,14 +6,34 @@ import (
 	"log"
 	"short-url/pkg/domain"
 	"short-url/pkg/interfaces/repository"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type URLInteractor struct {
-	URL repository.URL
+	URL         repository.URL
+	RedisClient *redis.Client
 }
 
 func (ui URLInteractor) SaveURL(ctx context.Context, url domain.URL) error {
-	err := ui.URL.Save(ctx, url)
+	key := url.Key
+	if url.IsKeySigned {
+		key = "-" + key
+	}
+
+	_, err := ui.RedisClient.Get(ctx, key).Result()
+	if err != nil {
+		log.Printf("Error: %v\n", err.Error())
+
+		if err == redis.Nil {
+			return errors.New("duplicate_request")
+		}
+
+		return errors.New("unable to save url")
+	}
+
+	err = ui.URL.Save(ctx, url)
 	if err != nil {
 		log.Printf("Error: %v\n", err.Error())
 
@@ -23,10 +43,25 @@ func (ui URLInteractor) SaveURL(ctx context.Context, url domain.URL) error {
 
 		return errors.New("unable to save url")
 	}
+
+	ui.RedisClient.Set(ctx, key, url.LongURL, time.Hour*24)
 	return nil
 }
 
 func (ui URLInteractor) FetchURL(ctx context.Context, url domain.URL) (domain.URL, error) {
+	key := url.Key
+	if url.IsKeySigned {
+		key = "-" + key
+	}
+
+	val, err := ui.RedisClient.Get(ctx, key).Result()
+	if err == nil {
+		url.LongURL = val
+		return url, nil
+	}
+
+	log.Println("Error: " + err.Error())
+
 	urlBody, err := ui.URL.Fetch(ctx, url)
 	if err != nil {
 		log.Printf("Error: %v\n", err.Error())
@@ -37,5 +72,7 @@ func (ui URLInteractor) FetchURL(ctx context.Context, url domain.URL) (domain.UR
 
 		return domain.URL{}, errors.New("unable to fetch url")
 	}
+
+	ui.RedisClient.Set(ctx, key, urlBody.LongURL, time.Hour*24)
 	return urlBody, nil
 }
